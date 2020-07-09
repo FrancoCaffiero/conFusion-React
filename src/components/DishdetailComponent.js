ff7010ff25620901c6678d52e343121900d3c2c6
FrancoCaffiero <FrancoCaffiero@gmail.com>
2020-07-09 16:16:30 +1200

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,8 +7,117 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Row,
+  Label,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Container,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
+
+// Form validations functions (validators)
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // Changes the state of isModalOpen used for Showing and hidding the modal form
+  toggleModal() {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+
+  // Handles the submit of the form
+  handleSubmit(values) {
+    console.log("Current state is: " + JSON.stringify(values));
+    alert("Current state is: " + JSON.stringify(values));
+    this.toggleModal();
+  }
+
+  render() {
+    return (
+      <>
+        <Button outline onClick={this.toggleModal}>
+          <span className="fa fa-pencil"></span> Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <Container>
+            <ModalBody>
+              <LocalForm onSubmit={this.handleSubmit}>
+                <Row className="form-group">
+                  <Label htmlFor="rating">Rating</Label>
+                  <Control.select
+                    model=".rating"
+                    id="rating"
+                    name="rating"
+                    className="form-control"
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Control.select>
+                </Row>
+                <Row className="form-group">
+                  <Label htmlFor="author">Your name</Label>
+                  <Control.text
+                    model=".author"
+                    id="author"
+                    name="author"
+                    className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      minLength: "Must be greater than 2 characters",
+                      maxLength: "Must be 15 characters or less",
+                    }}
+                  />
+                </Row>
+                <Row className="form-group">
+                  <Label htmlFor="comment">Comment</Label>
+                  <Control.textarea
+                    model=".comment"
+                    id="comment"
+                    name="comment"
+                    rows="6"
+                    className="form-control"
+                  />
+                </Row>
+                <Row>
+                  <Button type="submit" value="submit" color="primary">
+                    Submit
+                  </Button>
+                </Row>
+              </LocalForm>
+            </ModalBody>
+          </Container>
+        </Modal>
+      </>
+    );
+  }
+}
 
 // Returns a layout for a single dish
 function RenderDish({ dish }) {
@@ -60,10 +169,15 @@ function RenderComments({ comments }) {
             </li>
           ))}
         </ul>
+        <CommentForm />
       </div>
     );
   } else {
-    return <div></div>;
+    return (
+      <div>
+        <CommentForm />
+      </div>
+    );
   }
 }
 
